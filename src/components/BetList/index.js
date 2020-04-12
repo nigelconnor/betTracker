2b7abae97eaccdc9907245256847adb0ade8a48d
Nigel O Connor <treatyman@gmail.com>
2020-04-12 10:40:36 +0100

    .

--- src/components/BetList/index.js
+++ src/components/BetList/index.js
@@ -3,14 +3,6 @@ import Bet from "../Bet";
 
 export default class BetList extends Component {
   render() {
-    // let displayedBets = this.props.bets.map(b => (
-    //   <Bet
-    //     key={b._id}
-    //     bet={b}
-    //     deleteHandler={this.props.deleteHandler}
-    //     updateHandler={this.props.updateHandler}
-    //   />
-    //));
     return (
       <div>
         <h1>Bet History </h1>
